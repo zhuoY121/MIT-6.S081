f503146be0fb5b6c500d98c3c35dce78e5eb0b40
zhuoY121 <102482288+zhuoY121@users.noreply.github.com>
2023-07-27 17:50:35 -0400

    Finish vmprint

--- lab3-pageTable/kernel/vm.c
+++ lab3-pageTable/kernel/vm.c
@@ -432,3 +432,37 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  walk_print(pagetable, 0);
+}
+
+void
+walk_print(pagetable_t pagetable, int level)
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    
+    pte_t pte = pagetable[i];
+    
+    if(pte & PTE_V){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+
+      for (int j = 0; j < level; j++) {
+        printf(".. ");
+      }
+
+      printf("..%d: pte %p pa %p\n", i, pte, child);
+
+      // If child is not a leaf, then go to child table
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+        walk_print((pagetable_t)child, level+1);
+      }
+    }
+
+  }
+}
